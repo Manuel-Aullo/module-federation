7d4c8cba4cfc40062910de718b811fab6dd6958e
Manuel-Aullo <10832068+Manuel-Aullo@users.noreply.github.com>
2022-08-22 10:22:07 +0300

    Added basic folder

---
+++ basic/remote/src/App.js
@@ -0,0 +1,12 @@
+import LocalButton from "./Button";
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Remote</h1>
+    <h2>Button component served from here</h2>
+    <LocalButton />
+  </div>
+);
+
+export default App;

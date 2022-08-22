90a54b9b0bfbc918ffbdbe8a3f112e571e86c209
Manuel-Aullo <10832068+Manuel-Aullo@users.noreply.github.com>
2022-08-22 14:06:50 +0300

    Removed host and remote directories

--- remote/src/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({text, backgroundColor, color}) => {
-
-return(<button style={{backgroundColor:`${backgroundColor||"none"}`, color:`${color||"black"}`}}>{`${text || "Text for remote button"}`}</button>)};
-
-export default Button;
